ba965ce3c9246fce618fb56038917eca9f926359
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2019-10-07 14:22:55 +0100

    hooks called, object destructured

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import ProductContext from '../src/contexts/ProductContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -17,25 +18,35 @@ function App() {
 
 	return (
 		<div className="App">
+			<ProductContext.Provider value={{ products, addItem }}>
 			<Navigation cart={cart} />
+			</ProductContext.Provider>
 
 			{/* Routes */}
+			<ProductContext.Provider value={{ products, addItem }}>
 			<Route
 				exact
 				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
+				component = {Products}
+				// render={() => (
+				// 	<Products
+				// 		products={products}
+				// 		addItem={addItem}
+			 	/>
 				)}
 			/>
+			</ProductContext.Provider>
 
+			<ProductContext.Provider value={{ products, addItem }}>
 			<Route
 				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
+				component = {ShoppingCart}
+				// render={() => <ShoppingCart cart={cart} 
+				/>}
 			/>
+			</ProductContext.Provider>
 		</div>
+		
 	);
 }
 
